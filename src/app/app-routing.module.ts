f2407b94134925a2d35e81f3efc8348ec0872172
Diego Martín Campos <campos.martin.diego@gmail.com>
2022-03-04 14:00:48 +0100

    Nav, footer, index y sentimiento sin contenido

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IndexComponent } from './index/index.component';
+import { SentimientoComponent } from './sentimiento/sentimiento.component';
 
-const routes: Routes = [];
+//const routes: Routes = [];
+const routes: Routes = [
+  { path: 'index', component: IndexComponent },
+  { path: 'sentimiento', component: SentimientoComponent },
+  { path: '**',   redirectTo: '/index', pathMatch: 'full' }, //REDIRIGIR A INDEX
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
+import { IndexComponent } from './index/index.component';
+import { SentimientoComponent } from './sentimiento/sentimiento.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    FooterComponent,
+    IndexComponent,
+    SentimientoComponent
   ],
   imports: [
     BrowserModule,
