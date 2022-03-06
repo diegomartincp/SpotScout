14e6746d4e379f228d4a3575903a8950f4d89d19
Nicopb01 <nicolas.perez.beltran05@gmail.com>
2022-03-06 23:29:55 +0100

    imports bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { FooterComponent } from './footer/footer.component';
 import { IndexComponent } from './index/index.component';
 import { SentimientoComponent } from './sentimiento/sentimiento.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
