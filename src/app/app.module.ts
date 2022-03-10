2cfc61b0f3c8b824827fd6e8e1cbe0228373efee
Victor Perez <vtprz17@gmail.com>
2022-03-10 22:47:05 +0100

    cambios cscc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { ApiComunicationService } from './api-comunication.service'; //Importamos el servicio
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -22,7 +24,7 @@ import{ HttpClientModule } from '@angular/common/http';//
     AppRoutingModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [ApiComunicationService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
