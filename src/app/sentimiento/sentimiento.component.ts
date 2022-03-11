bf4c78db58a5a1e4b6e541cd21a929577dce9426
Victor Perez <vtprz17@gmail.com>
2022-03-11 17:28:49 +0100

    cambio imagen

--- src/app/sentimiento/sentimiento.component.ts
+++ src/app/sentimiento/sentimiento.component.ts
@@ -10,7 +10,9 @@ import { ApiComunicationService } from '../api-comunication.service'; //Importam
 //Componente sentimiento
 export class SentimientoComponent implements OnInit {
   //Variable resultado
-  resultado: any;
+  resultado : Number = 0;
+  imagen = "";
+  resultadoEsc ="";
 
   constructor(public service: ApiComunicationService) {
 
@@ -18,10 +20,21 @@ export class SentimientoComponent implements OnInit {
 
   ngOnInit(): void {
     //En inicio no hace nada
+    this.imagen = "../assets/images/experience.png";
   }
 
   async get_api_comunication_result(texto_a_analizar:string){
-    (await this.service.get_resultado_api(texto_a_analizar)).subscribe(data => (this.resultado = data.resultado));
+    (await this.service.get_resultado_api(texto_a_analizar)).subscribe(data => {this.resultado = data.resultado;
+    if(this.resultado > 0){
+      this.imagen = "../assets/images/feliz.png";
+      this.resultadoEsc = "Positivo"
+    }else if (this.resultado < 0){
+      this.imagen = "../assets/images/triste.png";
+      this.resultadoEsc = "Negativo"
+    }else{
+      this.imagen = "../assets/images/experience.png";
+      this.resultadoEsc = "Neutral"
+    }});
 
   }
 
