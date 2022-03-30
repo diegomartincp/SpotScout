3d292cdd792346b83195786168398fbffe0d09cf
Mario Carrasco Pérez <60732836+MarioCarrasco@users.noreply.github.com>
2022-03-30 14:09:01 +0200

    Cambios primera entrega
    Mejoras en get_resultado_api(). Cambios en get_api_comunication_result(). Uso de enviroment.ts para la url de la api. Quedan pendientes los imports de bootstrap

--- src/app/api-comunication.service.ts
+++ src/app/api-comunication.service.ts
@@ -3,10 +3,10 @@ import { HttpClient } from "@angular/common/http";  //Importamos HttpClient para
 
 import { API_RETURN } from './interfaces/API_RETURN'; //Importamos el objeto API_RETURN que no es mas que un objeto que define la estructura de lo devuielto por la API
 import { Observable } from 'rxjs';  //Para emplear el objeto observable
-
+import { environment } from '../environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 
 export class ApiComunicationService {
@@ -16,8 +16,10 @@ constructor(private servicio_api:HttpClient) {  //Inyectar la libreria para el s
 }
 
 //Pedimos el resultado a la API
-async get_resultado_api(texto_a_analizar:string) : Promise<Observable<API_RETURN>>{
-  const url = 'http://127.0.0.1:5000/api/analizar'; //La url de la api
+get_resultado_api(texto_a_analizar:string) : Observable<API_RETURN>{
+  const apiUrl = environment.api_url;
+  
+  const url = apiUrl+'api/analizar'; //La url de la api
   const body = { texto: texto_a_analizar }; //El cuerpo del JSON que se envía a la API
 
   return this.servicio_api.post<API_RETURN>(url,body) //Devolvemos el elemento devuelto por la API de tipo TASK
