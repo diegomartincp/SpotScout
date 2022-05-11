a267dee9a3abf24a191c21e3c41a88542b51c1e1
Nicopb01 <nicolas.perez.beltran05@gmail.com>
2022-05-11 13:01:05 +0200

    index

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiComunicationService } from '../api-comunication.service'; //Importamos el servicio
 
 @Component({
   selector: 'app-index',
@@ -6,10 +7,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./index.component.scss']
 })
 export class IndexComponent implements OnInit {
+  resultado: string = "";
+  m2: string = "";
+  medio: string = "";
+  imagen = "../assets/images/experience.png";
+  resultadoEsc = "0";
+  m2Esc = "0";
+  medioEsc = "0";
 
-  constructor() { }
+  constructor(
+    public service: ApiComunicationService
+  ) { }
 
   ngOnInit(): void {
   }
+  funcion_general(query:string){
+    this.service.servicio_delito_odio(query).subscribe(data => {
+      console.log(data.resultado);
+      this.resultadoEsc = data.resultado;
+    });
+    this.service.servicio_precio(query).subscribe(data => {
+      console.log(data.m2);
+      console.log(data.medio);
+      this.m2Esc = data.m2;
+      this.medioEsc = data.medio;
+    });
+  }
 
+  get_resultado_delito_odio(query:string){
+    console.log("hola 1")
+    this.service.servicio_delito_odio(query).subscribe(data => {
+      console.log(data.resultado);
+      this.resultadoEsc = data.resultado;
+    });
+    console.log("hola 2")
+  }
 }
+
+
