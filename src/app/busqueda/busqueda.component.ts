1f86247962b456d5261a9f311eb0d43695e67209
Diego Martín Campos <71278795+diegomartincp@users.noreply.github.com>
2022-05-30 12:58:44 +0200

    Gráfica odio

--- src/app/busqueda/busqueda.component.ts
+++ src/app/busqueda/busqueda.component.ts
@@ -24,11 +24,10 @@ export class BusquedaComponent implements OnInit {
   etiquetasRestaurantes : string[] = [];
 
   imagen = "../assets/images/experience.png";
-  resultadoEsc = "0";
+  resultadoEsc = 0;
   m2Esc = "0";
   medioEsc = "0";
   tweets_test=[];
-  //tweets_test=[0.0258,0,0.8402,0.8402,0.8402,-0.2732,0.4767,0,0,0,0,0,0,0.4767,0.4767,-0.8225,0,0,0,0.4767,0.4767,0.4767,0.4767,0,0.4497,0,0,0,0.807,0,0,0.4019,0.5267,0.5267,0.3818,0.7096,-0.4215,0,0.3182,-0.1655,0.4019,0,0.4019,0,0,0.6249,0.7787,-0.3182,0.3182,0,0,0,0,0,0,0.6833,0.5719,0,0.3182,0,0.5423,0.8172,0,0.3182,0.5423,0.5423,0,0,-0.1779,0.3818,0.4019,-0.2263,0.5423,-0.2263,-0.2263,0,0.4019,0,-0.0516,-0.0516,-0.0516,0.7096,0,0,0,0,0,0,0,0.3818,0,0.3612,0,0.7096,0,0,-0.5994,0.5501,0,-0.0258];
   query:any=""
   constructor(
     public service: ApiComunicationService,
@@ -36,8 +35,7 @@ export class BusquedaComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    //var tweets_funcion=[0,-0.6705,0.9055,0.765,0.8481,0,0,0,0,0,0,0,0.6486,0,0.7096,0.891,0,0,0.6908,0,0.347,-0.802,0,0.5719,0,0,0,0.8674,0.4215,0.749,-0.802,0.7184,0.7184,0.2732,0,0.9439,0,0,0.5719,0.7184,0,0.3818,0,0,0.0772,-0.6597,0,0,0.4767,0,-0.3612,0,0,-0.3612,0,0,0,0,0.5267,0.9231,0.5267,0,0,0.7964,0.8442,0.8442,0.6249,0.4019,0,0.6249,0.0772,0.4215,0.7156,0,0,0,-0.296,0,0.9094,0.9094,0,0,0,0,0.2235,0,0,-0.4215,0.0258,0,0.765,0,0,0.875,0.4019,0.4019,0.128,0.128,0.4767,0.4019];
-    //this.chart2(tweets_funcion);
+    //this.chart_odio();
 
     this._Activatedroute.paramMap.subscribe(params => {
       this.query = params.get('query');
@@ -49,45 +47,43 @@ export class BusquedaComponent implements OnInit {
 
   funcion_general(query:string){
     this.service.servicio_busqueda(query).subscribe(data => {
+      //Sentimiento de odio
       console.log(data.porcentaje_odio);
-      this.resultadoEsc = data.porcentaje_odio;
+      this.resultadoEsc = <number><unknown>data.porcentaje_odio;
+      var no_odio=1-this.resultadoEsc;
+      this.chart_odio(this.resultadoEsc,no_odio);
+
+
+      //Precio medio y M2
       console.log(data.precio_m2);
       console.log(data.precio_viviendas);
       this.m2Esc = data.precio_m2;
       this.medioEsc = data.precio_viviendas;
 
+      //Viviendas alquiler y venta
       console.log(data.num_viviendas_venta);
       console.log(data.num_viviendas_alquiler);
       this.numVivVent = data.num_viviendas_venta;
       this.numVivAlq = data.num_viviendas_alquiler;
 
       //Los restaurantes tienen tres array de string: nombre, puntuacion, etiquetas
-
-      //this.nombresResEsc = data.nombre;
-      //this.valoracionResEsc = data.puntuacion;
-      //this.etiquetasResEsc = data.etiquetas;
-
       //Guardamos las variables con los string
       var nombres=data.nombre;
       var valoraciones=data.puntuacion;
       var etiquetas=data.etiquetas;
-      //Eliminamos los elementos del array [ " ]
-      //var nombres=nombres.replace('"', '').replace('[', '').replace(']', '');
-      //var valoraciones=valoraciones.replace('"', '').replace('[', '').replace(']', '');
-      //var etiquetas=etiquetas.replace('"', '').replace('[', '').replace(']', '');
-      console.log(nombres);
-      //Ahora que lo tenemos limpio podemos castear a un array de strings
+
+      //castear a un array de strings
       this.nombresRestaurantes=nombres.split(",");
       this.valoracionesRestaurantes=valoraciones.split(",");
       this.etiquetasRestaurantes=etiquetas.split(",");
       console.log(this.nombresRestaurantes);
       console.log(this.valoracionesRestaurantes);
       console.log(this.etiquetasRestaurantes);
-      //Para todos los elementos de los 3 array hacemos
+
+      //Para todos los elementos de los 3 array hacemos la limpieza de los elementos [, ], "
       for(var i=0;i<this.nombresRestaurantes.length;i++){
         this.nombresRestaurantes[i]=this.nombresRestaurantes[i].replace('"', '').replace('"', '').replace('[', '').replace(']', '')
         this.valoracionesRestaurantes[i]=this.valoracionesRestaurantes[i].replace('"', '').replace('"', '').replace('[', '').replace(']', '')
-        //this.etiquetasRestaurantes[i]=this.etiquetasRestaurantes[i].replace('"', '').replace('"', '').replace('[', '').replace(']', '')
       }
       for(var i=0;i<this.etiquetasRestaurantes.length;i++){
         this.etiquetasRestaurantes[i]=this.etiquetasRestaurantes[i].replace('"', '').replace('"', '').replace('[', '').replace(']', '')
@@ -97,14 +93,15 @@ export class BusquedaComponent implements OnInit {
       this.tweets= data.ultimos_100;
       this.tweets=String(this.tweets).replace('"', '').replace('[', '').replace(']', '');  //Quitamos todos los elementos del array " ] [
       var tweets_number_array = <Array<number>><unknown>this.tweets.split(","); //Convertimos la variable con el string en un array de number al castear
-      //Generamos la gráfica
-      this.chart2(tweets_number_array);
+
+      //Generamos la gráfica de tweets
+      this.chart_tweets(tweets_number_array);
     });
   }
 //CHARTS
 
 //CHART 2
-chart2(tweets: Number[]){
+chart_tweets(tweets: Number[]){
   var labels_=Array.from(Array(100).keys());
 
   var myLineChart = new Chart("myAreaChart", {
@@ -160,6 +157,24 @@ chart2(tweets: Number[]){
 
     }
   })
+}
+
+chart_odio(odio: number, no_odio: number){
+  var myPieChart = new Chart("myPieChart", {
+    type: 'doughnut',
+    data: {
+      labels: ["Odio"],
+      datasets: [{
+        data: [odio,no_odio],
+        backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
+        hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
+        hoverBorderColor: "rgba(234, 236, 244, 1)",
+      }],
+    },
+    options: {
+      maintainAspectRatio: false,
+    },
+  });
 }
 
 }
