0bd734fa81f1e313772c7d062e19177439795639
Victor Perez <vtprz17@gmail.com>
2022-06-03 03:02:19 +0200

    fondo de busqueda

--- src/app/busqueda/busqueda.component.ts
+++ src/app/busqueda/busqueda.component.ts
@@ -52,23 +52,14 @@ export class BusquedaComponent implements OnInit {
   funcion_general(query:string){
     this.mostrar='visible'
     this.tamano = 8
+
     Swal.fire({
-      title: '<strong>HTML <u>example</u></strong>',
+      title: 'Realizando la busqueda',
       icon: 'info',
       html:
-        'You can use <b>bold text</b>, ' +
-        '<a href="//sweetalert2.github.io">links</a> ' +
-        'and other HTML tags',
-      showCloseButton: true,
-      showCancelButton: true,
-      focusConfirm: false,
-      confirmButtonText:
-        '<i class="fa fa-thumbs-up"></i> Great!',
-      confirmButtonAriaLabel: 'Thumbs up, great!',
-      cancelButtonText:
-        '<i class="fa fa-thumbs-down"></i>',
-      cancelButtonAriaLabel: 'Thumbs down'
+        "<div class='spinner-border float-right text-primary' style='margin: 5px' role='status'></div>"
     })
+
     this.m2Esc = "";
     this.medioEsc = "";
     this.numVivVent = "";
