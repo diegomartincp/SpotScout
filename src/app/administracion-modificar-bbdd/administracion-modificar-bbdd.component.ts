111bcb731687385fad1ffd37eff7c6a11ab7b041
Victor Perez <vtprz17@gmail.com>
2022-06-04 20:13:40 +0200

    css pagina tweets admin

--- src/app/administracion-modificar-bbdd/administracion-modificar-bbdd.component.ts
+++ src/app/administracion-modificar-bbdd/administracion-modificar-bbdd.component.ts
@@ -21,21 +21,33 @@ export class AdministracionModificarBBDDComponent implements OnInit {
 
   cambiarCantidad(cantidad:string){
     this.numeroCantidad = parseInt(cantidad);
-    this.service.servicio_cambiar_numero_tweets(this.numeroCantidad).subscribe();
-    Swal.fire({
-      title: 'El numero de tweets ha sido modificado',
-      icon: 'success',
-      showCancelButton: false,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Continuar'
-    }).then((result) => {
-      if (result.isConfirmed) {
-        window.location.reload();
-      }
-    })
-    console.log(this.numeroCantidad);
+    if(this.numeroCantidad > 19){
+      this.service.servicio_cambiar_numero_tweets(this.numeroCantidad).subscribe();
+      Swal.fire({
+        title: 'El numero de tweets ha sido modificado',
+        icon: 'success',
+        showCancelButton: false,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Continuar'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          window.location.reload();
+        }
+      })
+      console.log(this.numeroCantidad);
+    }else{
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'El numero de tweets debe ser 20 o mayor',
+      })
+    }
+
+
+
   }
+
 
 
 }
